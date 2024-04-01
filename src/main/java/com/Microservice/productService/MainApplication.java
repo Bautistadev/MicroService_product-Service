b0f13db56890e3e9368db103d4b4eb036ffbe7aa
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2024-04-01 15:54:59 -0300

    .

--- src/main/java/com/Microservice/productService/MainApplication.java
+++ src/main/java/com/Microservice/productService/MainApplication.java
@@ -1,26 +1,15 @@
 package com.Microservice.productService;
 
-import com.Microservice.productService.Entity.BrandEntity;
-import com.Microservice.productService.Repository.BrandRepository;
-import com.Microservice.productService.Repository.ProductRepository;
-import com.Microservice.productService.Service.BrandService;
-import com.Microservice.productService.Service.ProductService;
-import com.Microservice.productService.model.*;
-import com.Microservice.productService.model.BrandRequestDTO;
-import com.Microservice.productService.model.ProductRequestDTO;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -29,8 +18,6 @@ import java.time.LocalDate;
 @EnableFeignClients
 public class MainApplication implements CommandLineRunner {
 
-	@Value(value = "${app.test.property}")
-	private String pro;
 
 	public static void main(String[] args) {
 
@@ -41,6 +28,6 @@ public class MainApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		System.out.println(pro);
+
 	}
 }
